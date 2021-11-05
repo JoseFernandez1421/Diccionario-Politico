fe554a08bc11866ec8cfd0b3538aa9c1d3a5fb84
José Martín Fernández Cubas <90362934+JoseFernandez1421@users.noreply.github.com>
2021-11-05 04:32:37 -0500

    Add files via upload

--- frontend/politipedia-app/src/app/shared/service/politipedia.service.ts
+++ frontend/politipedia-app/src/app/shared/service/politipedia.service.ts
@@ -12,26 +12,51 @@ export class PolitipediaService {
 
   }
 
-  public getPoliticos(body: any) {
-    return this.http.post<any>(this.createRoute('api/politicos'),body, this.generateHeaders());
+  public getPoliticos(nombre: string) {
+    return this.http.get<any>(this.createRoute(`api/politicos/${nombre}`), this.generateHeaders());
   }
 
-  public getDetalle(body: any) {
-    return this.http.post<any>(this.createRoute('api/politico'),body, this.generateHeaders());
+  public getDetalle(dni: string) {
+    return this.http.get<any>(this.createRoute(`api/politico/${dni}`), this.generateHeaders());
   }
 
-  public getMilitancias(body: any) {
-    return this.http.post<any>(this.createRoute('api/militancias'),body, this.generateHeaders());
+  public getMilitancias(dni: string) {
+    return this.http.get<any>(this.createRoute(`api/militancias/${dni}`), this.generateHeaders());
   }
 
-  public getNotasPeriodisticas(body: any) {
-    return this.http.post<any>(this.createRoute('api/notas_periodisticas'),body, this.generateHeaders());
+  public getNotasPeriodisticas(dni: string) {
+    return this.http.get<any>(this.createRoute(`api/notas_periodisticas/${dni}`), this.generateHeaders());
   }
 
-  public getCargosPublicos(body: any) {
-    return this.http.post<any>(this.createRoute('api/cargos_publicos'),body, this.generateHeaders());
+  public getCargosPublicos(dni: string) {
+    return this.http.get<any>(this.createRoute(`api/cargos_publicos/${dni}`), this.generateHeaders());
   }
 
+
+  public newNotasPeriodisticas(body: any) {
+    console.log("link: "+body);
+    return this.http.post<any>(this.createRoute('api/notas_periodisticas/new'),body, this.generateHeaders());
+  }
+
+  public deleteNotasPeriodisticas(codigo_nota: string, dni: string) {
+    console.log("dni: "+dni);
+    console.log("codigo_nota: "+codigo_nota);
+    //return this.http.delete<any>(this.createRoute(`api/notas_periodisticas/delete/${codigo_nota}/${dni}`), this.generateHeaders());
+
+
+    return this.http.delete(this.createRoute(`api/notas_periodisticas/delete/${codigo_nota}/${dni}`)) 
+    .subscribe((data)=>{ 
+      console.log(data); //data will actually be an Object sent from Express
+    })
+
+
+  }
+
+
+
+
+
+  
   private createRoute = (route: string) => {
     return `${environment.urlApi}/${route}`;
   }
