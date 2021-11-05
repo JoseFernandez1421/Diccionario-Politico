c940c6887253aabccd45827b0432e5416d81386a
José Martín Fernández Cubas <90362934+JoseFernandez1421@users.noreply.github.com>
2021-11-05 04:27:51 -0500

    Add files via upload

--- frontend/politipedia-app/src/app/app-routing.module.ts
+++ frontend/politipedia-app/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CargosPublicosComponent } from './cargos-publicos/cargos-publicos.component';
+import { DetalleComponent } from './detalle/detalle.component';
+import { HomeComponent } from './home/home.component';
+import { MilitanciasComponent } from './militancias/militancias.component';
+import { NotasPeriodisticasNewComponent } from './notas-periodisticas-new/notas-periodisticas-new.component';
+import { NotasPeriodisticasComponent } from './notas-periodisticas/notas-periodisticas.component';
+import { PoliticosListaComponent } from './politicos-lista/politicos-lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'politicos-lista', component: PoliticosListaComponent},
+  {path: 'politicos-lista/:nombre', component: PoliticosListaComponent},
+  {path: 'detalle/:dni', component: DetalleComponent},
+  {path: 'militancias/:dni', component: MilitanciasComponent},
+  {path: 'notas-periodisticas/:dni', component: NotasPeriodisticasComponent},
+  {path: 'cargos-publicos/:dni', component: CargosPublicosComponent},
+  {path: 'notas-periodisticas-new/:dni', component: NotasPeriodisticasNewComponent },
+  
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
