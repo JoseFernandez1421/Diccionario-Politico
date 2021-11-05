6553a40da1f686517feddec8ba398d0835ff232f
José Martín Fernández Cubas <90362934+JoseFernandez1421@users.noreply.github.com>
2021-11-05 04:29:10 -0500

    Add files via upload

--- frontend/politipedia-app/src/app/app.module.ts
+++ frontend/politipedia-app/src/app/app.module.ts
@@ -14,18 +14,21 @@ import { MatSliderModule } from '@angular/material/slider';
 import {MatTableModule} from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
 
-import { HomeComponent } from './home/home.component';
-import { PoliticosListaComponent } from './politicos-lista/politicos-lista.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+
 import {MatInputModule} from '@angular/material/input';
 
 import {MatDividerModule} from '@angular/material/divider';
+import { AppRoutingModule } from './app-routing.module';
+
 import { DetalleComponent } from './detalle/detalle.component';
 import { MilitanciasComponent } from './militancias/militancias.component';
 import { NotasPeriodisticasComponent } from './notas-periodisticas/notas-periodisticas.component';
 import { CargosPublicosComponent } from './cargos-publicos/cargos-publicos.component';
-
+import { HomeComponent } from './home/home.component';
+import { PoliticosListaComponent } from './politicos-lista/politicos-lista.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotasPeriodisticasNewComponent } from './notas-periodisticas-new/notas-periodisticas-new.component';
 
 
 
@@ -39,7 +42,8 @@ import { CargosPublicosComponent } from './cargos-publicos/cargos-publicos.compo
     DetalleComponent,
     MilitanciasComponent,
     NotasPeriodisticasComponent,
-    CargosPublicosComponent
+    CargosPublicosComponent,
+    NotasPeriodisticasNewComponent
   ],
   imports: [
     BrowserModule,
@@ -53,16 +57,7 @@ import { CargosPublicosComponent } from './cargos-publicos/cargos-publicos.compo
     MatInputModule,
     BrowserAnimationsModule,
     MatDividerModule,
-    RouterModule.forRoot([
-      {path: '', redirectTo: '/home', pathMatch: 'full'},
-      {path: 'home', component: HomeComponent},
-      {path: 'politicos-lista', component: PoliticosListaComponent},
-      {path: 'politicos-lista/:nombre', component: PoliticosListaComponent},
-      {path: 'detalle/:dni', component: DetalleComponent},
-      {path: 'militancias/:dni', component: MilitanciasComponent},
-      {path: 'notas-periodisticas/:dni', component: NotasPeriodisticasComponent},
-      {path: 'cargos-publicos/:dni', component: CargosPublicosComponent},
-    ])
+    AppRoutingModule 
   ],
   providers: [],
   bootstrap: [AppComponent],
