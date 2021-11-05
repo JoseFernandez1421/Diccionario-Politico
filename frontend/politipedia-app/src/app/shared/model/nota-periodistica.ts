f5a50ec22a4a7fc6a9c493035addf06973ffd383
José Martín Fernández Cubas <90362934+JoseFernandez1421@users.noreply.github.com>
2021-11-05 04:32:10 -0500

    Add files via upload

--- frontend/politipedia-app/src/app/shared/model/nota-periodistica.ts
+++ frontend/politipedia-app/src/app/shared/model/nota-periodistica.ts
@@ -1,7 +1,8 @@
 export class NotaPeriodistica {
-  
+  public codigo_nota: string = "";
   public dni: string = "";
   public resumen: string = "";
   public link: string = "";
+  public recorte: string = "";
 
 }
